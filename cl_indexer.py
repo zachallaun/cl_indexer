55d6d707a8732fa82867dc4d04c94612eb7d3e9e
swoopej <swoopej@gmail.com>
2012-11-12 17:00:49 -0500

    pre-venv

--- cl_indexer.py
+++ cl_indexer.py
@@ -6,110 +6,125 @@ import datetime
 
 #global list of neighborhood names to track
 NEIGHBORHOOD_LIST = [
-			'Williamsburg',
-			'Bushwick',
-			'Greenpoint',
-			'Park Slope',
-			'Fort Green', #misspelling will catch the correct one too...#megabonus
-			'Prospect Heights',
-			'Crown Heights',
-			'Clinton Hill',
-			'DUMBO',
+			'Bay Ridge',
+			'Bay ridge',
 			'Bed-Stuy',
 			'BedStuy',
 			'Bedford-Stuyvesant',
+			'Bedstuy',
 			'Bensonhurst',
+			'Brooklyn Heights',
+			'Brooklyn heights',
+			'Bushwick',
 			'Canarsie',
+			'Carrol Gardens',
+			'Carroll Gardens',
+			'Carrol Garden',
+			'Clinton Hill',
+			'Clinton hill',
+			'Crown Heights',
+			'Crown heights'
+			'Ditmas Park',
+			'Ditmas park',
+			'DUMBO',
+			'East Williamsburg',
+			'Fort Green', #misspelling will catch the correct one too...#megabonus
+			'Greenpoint',
+			'Park Slope',
+			'Prospect Heights',
 			'Red Hook',
+			'Red hook',
+			'Sunset Park',
+			'Sunset park',
+			'Vinegar Hill',
+			'Vinegar hill',
+			'Williamsburg',
 			]
 
+CL_URL = 'http://newyork.craigslist.org/brk/aap/index.rss'
 
-class cl_data_parser():
 
-	def __init__(self):
-		pass
 
-	#def event_scheduler(self):
+def run_scheduler():
+	x = 0
+	while True:
+		write_data()
+		parse_data()
+		print 'iteration number: ', x
+		time.sleep(10)
+		x += 1
 
-	#pings the craigslist rss url, writes the data to a file
-	#defaulted to the cl brooklyn url
-	def write_data(self):
-		data = urllib2.urlopen('http://newyork.craigslist.org/brk/aap/index.rss')
-		outstream = open('index.rss', 'wt')
+#pings the craigslist rss url, writes the data to a file
+#defaulted to the cl brooklyn url
+def write_data():
+	data = urllib2.urlopen(CL_URL)
+	with open('index.rss', 'wt') as outstream:
 		for line in data:
-			if line.find('<?xml version="1.0" encoding="iso-8859-1"?>') != -1:
-				pass
-			else :
+			if line.find('<?xml version="1.0" encoding="iso-8859-1"?>') == -1:
 				outstream.write(line)
 
 
+def parse_data():
+	with open('index.rss', 'rt') as file:
+		read_string = file.read()
+	items = BeautifulSoup(read_string, 'xml').findAll('item')
+	for item in items:
+		title = item.title.string
+		index = title.rfind('(')
+		if index != -1: #error check if there is no parens for the neighborhood
+			title = title[index:] #slice the string at the opening paren
+			url = item.link.string
+			make_list(title, url)
 
-	def parse_data(self):
-		in_stream = open('index.rss', 'rt').read()
-		items = BeautifulSoup(in_stream, 'xml').findAll('item')
-		for item in items:
-			title = str(item.title)
-			index = title.find('(')
-			if index != -1: #error check if there is no parens for the neighborhood
-				title = title[index:] #slice the string at the opening paren
-				url = item.link.string
-				list_for_item = self.make_list(title, url)
-
-	def make_list(self, item, url):
-		list_for_item = []
-		list_for_item.append(url)
-		list_for_item.append(self.find_nabe(item))
-		list_for_item.append(self.find_price(item))
-		list_for_item.append(self.find_bedrooms(item))
-		list_for_item.append(datetime.date)
-		url = str(list_for_item[0])
-		neighborhood = str(list_for_item[1])
-		price = list_for_item[2]
-		bedrooms = list_for_item[3]
+def make_list(item, url):
+	if not (get_by_url(url)):
+		neighborhood = str(find_nabe(item)) #have to cast to str for db commit
+		price = find_price(item)
+		bedrooms = find_bedrooms(item)
 		now = datetime.datetime.now()
-		test_tuple = (url, neighborhood, price, bedrooms, now)
-		conn.execute('INSERT INTO listings VALUES(?,?,?,?,?)', test_tuple) 
+		if (neighborhood != [] and price != [] and bedrooms != -1):
+			listing = (url, neighborhood, price, bedrooms, now)
+			conn.execute('INSERT INTO listings VALUES(?,?,?,?,?)', listing)
 
-	#loops through nabe list to check if any appear in the item
-	def find_nabe(self, item):
-		neighborhoods_in_item = []
-		for neighborhood in NEIGHBORHOOD_LIST:
-			if item.find(neighborhood) != -1 or item.find(neighborhood.lower()) != -1 or item.find(neighborhood.upper()) != -1:
-				neighborhoods_in_item.append(neighborhood)
-		return neighborhoods_in_item
+def get_by_url(url):
+	return (conn.execute('SELECT * FROM listings WHERE url=(?)', (url,))).fetchall()
 
-	def find_price(self, item):
-		price = ''
-		index = item.find('$')
-		if index != -1:
-			item = item[index:]
-			for i in range(5):
-				if item[i + 1].isdigit():
-					price += item[i + 1]
-		return price
+#loops through nabe list to check if any appear in the item
+def find_nabe(item):
+	return [x for x in NEIGHBORHOOD_LIST if any(item.find(y) != -1 for y in [x, x.lower(), x.upper()])]
 
 
+def find_price(item):
+	price = ''
+	index = item.rfind('$')
+	if index != -1:
+		item = item[index + 1:]
+		for i in range(5):
+			if i >= len(item): break
+			elif not(item[i].isdigit()): break
+			else: 
+				price += item[i]
+	print 'price: ', price
+	return price
 
-	def find_bedrooms(self, item):
-		bedrooms = ''
-		index = item.find('$')
-		item = item[index:]
-		index = item.find('bd')
-		if index != -1:
-			bedrooms = item[index -1]
-		return bedrooms
 
+
+def find_bedrooms(item):
+	bedrooms = ''
+	index = item.rfind('$') #rfind to find the last $
+	item = item[index:]
+	index = item.find('bd')
+	if index != -1:
+		bedrooms = item[index -1]
+	return bedrooms
 
 
 
 conn = sqlite3.connect('test_db.db')
 cursor = conn.cursor()
-cursor.execute(''' drop table if exists listings ''')
-cursor.execute(''' CREATE TABLE listings
-					(url text, neighborhood text, price integer, bedrooms integer, today integer )''')
-parser = cl_data_parser()
-parser.write_data()
-parser.parse_data()
+run_scheduler()
 conn.commit()
 conn.close()
+
+
 
